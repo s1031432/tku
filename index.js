51cf90047ee0e74ed37a8479cf9cb783e1b4d90a
Huang, Yi-Tang <ty80517@gmail.com>
2022-06-17 15:02:29 +0800

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+    // event.1 - onclick
+    $(".ant-tag").on("click", (event)=>{
+        $("#span_row > .ant-tag").attr("class", "ant-tag span_button");
+        $(event.target).attr("class", "ant-tag span_button ant-tag-checkable-checked");
+    });
+    $(".span_button").on("click", (event) => {
+        for(var i=0;i<$(".span_button").length;i++){
+            console.log(i);
+            if( $(".span_button").eq(i).hasClass("ant-tag-checkable-checked") ){
+                console.log("2");
+                $(".span_table").hide();
+                $(".span_table").eq(i).show();
+            }
+                
+        }
+    });
+    // event.2 - mouseenter
+    $("tr").on("mouseenter", (event) => {
+        console.log(this);
+        $(event.target.parentElement).css("background-color", "rgba(193, 215, 230, 0.6)");
+    });
+    // event.3 - mouseout
+    $("tr").on("mouseout", (event) => {
+        console.log(this);
+        $(event.target.parentElement).css("background-color", "");
+    });
+}
