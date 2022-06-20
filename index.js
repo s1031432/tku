a74a8ccee0ef94bbbba2cd2aa5c0d97ee4c1b33a
Huang, Yi-Tang <ty80517@gmail.com>
2022-06-20 23:52:26 +0800

    split four page

--- index.js
+++ index.js
@@ -6,24 +6,18 @@ window.onload = () => {
     });
     $(".span_button").on("click", (event) => {
         for(var i=0;i<$(".span_button").length;i++){
-            console.log(i);
             if( $(".span_button").eq(i).hasClass("ant-tag-checkable-checked") ){
-                console.log("2");
                 $(".span_table").hide();
                 $(".span_table").eq(i).show();
             }
-                
         }
     });
     // event.2 - mouseenter : table hover
     $("tr").on("mouseenter", (event) => {
-        console.log(this);
         $(event.target.parentElement).css("background-color", "rgba(193, 215, 230, 0.6)");
     });
     // event.3 - mouseout : table hover
     $("tr").on("mouseout", (event) => {
-        console.log(this);
         $(event.target.parentElement).css("background-color", "");
     });
-    $("#span_row > .ant-tag").eq(0).click();
 }
